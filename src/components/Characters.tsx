774fec7bd145581b4bd182e40260465b4a64e298
bkkom <seoy1108@gmail.com>
2023-09-24 18:01:49 +0900

    Chore: Loading state

--- src/components/Characters.tsx
+++ src/components/Characters.tsx
@@ -9,26 +9,36 @@ interface ICharacters {
 
 const Characters = () => {
   const [characters, setCharacters] = useState<ICharacters[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const getData = async () => {
+    const response = await fetch('https://disney_api.nomadcoders.workers.dev/characters');
+    const json = await response.json();
+    setCharacters(json.slice(0, 100));
+    setLoading(false);
+  };
 
   useEffect(() => {
-    (async () => {
-      const response = await fetch('https://disney_api.nomadcoders.workers.dev/characters');
-      const json = await response.json();
-      setCharacters(json.slice(0, 100));
-    })();
+    getData();
   }, []);
 
   console.log(characters);
 
   return (
     <>
-      <div>Characters</div>
-      {characters.map((character) => (
-        <ul key={character.id}>
-          <li>{character.name}</li>
-          <img src={character.imageUrl} />
-        </ul>
-      ))}
+      {loading ? (
+        'Loading...'
+      ) : (
+        <>
+          <div>Characters</div>
+          {characters.map((character) => (
+            <ul key={character.id}>
+              <li>{character.name}</li>
+              <img src={character.imageUrl} />
+            </ul>
+          ))}
+        </>
+      )}
     </>
   );
 };
