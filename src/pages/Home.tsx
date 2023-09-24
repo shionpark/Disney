04880e9062dad306f22395e83b40b1590c6cf2bd
bkkom <seoy1108@gmail.com>
2023-09-25 03:20:02 +0900

    Style: Home 화면 스타일 적용

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Characters } from '@/components';
+import styled from 'styled-components';
 
 const Home = () => {
   return (
     <>
+      <Title>Disney Characters</Title>
       <Characters />
     </>
   );
 };
+
+export const Title = styled.h1`
+  font-size: 52px;
+  text-align: center;
+  color: ${(props) => props.theme.accentColor};
+`;
 
 export default Home;
